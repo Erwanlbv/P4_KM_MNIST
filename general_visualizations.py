3b6e203b86fe051a0af9d4f2051a750659d70d27
Erwanlbv <er.leblevec@gmail.com>
2022-02-01 23:59:34 +0100

    Ajout de la fonction association entre les regroupements et les vraie classes

--- general_visualizations.py
+++ general_visualizations.py
@@ -11,8 +11,10 @@ def display_histos(pred_labels, true_labels):
     axs[0].hist(true_labels)
 
     axs[1].set_title('Histogramme prédit')
-    axs[1].hist(pred_labels, range=(0, 10 * (len(np.unique(pred_labels)) < 10) +
-                                    len(np.unique(pred_labels)) * (len(np.unique(pred_labels)) >= 10)))
+    axs[1].hist(pred_labels,
+                range=(0, 10 * (len(np.unique(pred_labels)) < 10) + len(np.unique(pred_labels)) * (len(np.unique(pred_labels)) >= 10)),
+                rwidth=0.5,
+                )
 
     fig.show()
 
@@ -26,6 +28,18 @@ def get_histo(km, true_labels, n_clusters):
         # Affiche le centre du cluster
 
 
+def get_label_real_class(true_labels, pred_labels):
+    all_labels = np.unique(pred_labels)
+    index_classes = []
+    label_equivalence = []
+
+    for label in all_labels:
+        index_classes.append(np.where(pred_labels == label))
+        res = [np.sum(true_labels[index_classes] == i) for i in range(len(all_labels))]
+        label_equivalence.append(res[np.argmax(res)])
+
+    print('Liste des équivalences', label_equivalence) # label_equivalence[0] correspond à la classe 'réelle' du 1er regroup. [1] du 2e..
+    return label_equivalence
 
 
 
