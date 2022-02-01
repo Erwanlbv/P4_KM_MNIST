36673bb93281a107b4a0103e30d9d37543a175b5
Erwanlbv <er.leblevec@gmail.com>
2022-02-01 23:21:35 +0100

    Ajout des fonctions histogrammes pour représenter les résultats

---
+++ general_visualizations.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def display_histos(pred_labels, true_labels):
+
+    fig, axs = plt.subplots(2, figsize=(11, 7))
+    fig.suptitle('Histogrammes réel et prédit, pour ' + str(len(true_labels)) + 'images', fontsize=15)
+
+    axs[0].set_title('Histogramme réel')
+    axs[0].hist(true_labels)
+
+    axs[1].set_title('Histogramme prédit')
+    axs[1].hist(pred_labels, range=(0, 10 * (len(np.unique(pred_labels)) < 10) +
+                                    len(np.unique(pred_labels)) * (len(np.unique(pred_labels)) >= 10)))
+
+    fig.show()
+
+
+def get_histo(km, true_labels, n_clusters):
+
+    for i in range(n_clusters):
+        fig, ax = plt.subplots(3, fontsize=15)
+        fig.suptitle('Histogramme du cluster n° ' + str(i))
+
+        # Affiche le centre du cluster
+
+
+
+
+
